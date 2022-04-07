fd21033007328dd0f5a556ddf31eebb1209d2d59
Ferry Tjahjadi <ferryalvianto@gmail.com>
2022-04-06 18:50:32 -0700

    added fonts

--- app/src/main/java/com/example/mvvm_run1/dao/NoteDAO.java
+++ app/src/main/java/com/example/mvvm_run1/dao/NoteDAO.java
@@ -4,46 +4,48 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
 import com.example.mvvm_run1.model.Note;
 
 import java.util.List;
+
 @Dao
-public interface        NoteDAO {
+public interface NoteDAO {
 
-        @Insert
-        void insertNote(Note note);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertNote(Note note);
 
-        @Update
-        void updateNote(Note note);
+    @Update
+    void updateNote(Note note);
 
-        @Delete
-        void deleteNote(Note note);
+    @Delete
+    void deleteNote(Note note);
 
-        @Query("SELECT * FROM note_table")
-        List<Note> findAllNotes();
+    @Query("SELECT * FROM note_table")
+    List<Note> findAllNotes();
 
-        @Query("SELECT * FROM note_table WHERE notetitle =:title AND notecontent=:content")
-        List<Note> getNoteByTitleContent(String title,String content);
+    @Query("SELECT * FROM note_table WHERE notetitle =:title AND notecontent=:content")
+    List<Note> getNoteByTitleContent(String title, String content);
 
-        @Query("SELECT * FROM note_table WHERE usercreatorid =:usercreatorid ORDER BY noteid DESC")
-        List<Note> getAllNotesById(int usercreatorid);
+    @Query("SELECT * FROM note_table WHERE usercreatorid =:usercreatorid ORDER BY noteid DESC")
+    List<Note> getAllNotesById(int usercreatorid);
 
-        @Query("SELECT * FROM note_table WHERE usercreatorid =:usercreatorid ORDER BY noteid DESC")
-        LiveData<List<Note>> getNotesById(int usercreatorid);
+    @Query("SELECT * FROM note_table WHERE usercreatorid =:usercreatorid ORDER BY noteid DESC")
+    LiveData<List<Note>> getNotesById(int usercreatorid);
 
-        @Query("SELECT notetitle FROM note_table WHERE noteid =:noteid")
-        LiveData<String> getNoteTitleByNoteId(int noteid);
+    @Query("SELECT notetitle FROM note_table WHERE noteid =:noteid")
+    LiveData<String> getNoteTitleByNoteId(int noteid);
 
-        @Query("SELECT notecontent FROM note_table WHERE noteid =:noteid")
-        LiveData<String> getNoteContentByNoteId(int noteid);
+    @Query("SELECT notecontent FROM note_table WHERE noteid =:noteid")
+    LiveData<String> getNoteContentByNoteId(int noteid);
 
-        @Query("UPDATE note_table SET notetitle=:title , notecontent=:content WHERE noteid =:id")
-        void updateNoteById(String title,String content,int id);
+    @Query("UPDATE note_table SET notetitle=:title , notecontent=:content WHERE noteid =:id")
+    void updateNoteById(String title, String content, int id);
 
-        @Query("DELETE FROM note_table WHERE noteid = :noteid")
-        void deleteNoteById(int noteid);
+    @Query("DELETE FROM note_table WHERE noteid = :noteid")
+    void deleteNoteById(int noteid);
 
 }
